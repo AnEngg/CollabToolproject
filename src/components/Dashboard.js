817dcddc8849ba4386b9274f57f70cfe912ca4dd
Mauli Bondare <bondaremauli10@gmail.com>
2025-03-23 15:53:52 +0530

    Initial commit

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -104,7 +104,7 @@ const Dashboard = () => {
     return (
       <div className="d-flex justify-content-center align-items-center vh-100">
         <div className="spinner-border text-light" role="status">
-          <span className="visually-hidden">Loading...</span>
+          <span className="visually-hidden">Loading..</span>
         </div>
       </div>
     );
